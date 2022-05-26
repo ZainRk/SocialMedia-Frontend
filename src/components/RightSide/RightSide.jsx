393b6aed0b49ad37fdedaa9597da5bea03be49ac
Theodor Bellash <theobellash@gmail.com>
2022-05-26 08:08:11 +0100

    added routes and links

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -6,13 +6,14 @@ import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
 import ShareModal from "../ShareModal/ShareModal";
+import { Link } from "react-router-dom";
 
 const RightSide = () => {
   const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="RightSide">
       <div className="navIcons">
-        <img src={Home} alt="" />
+        <Link to='/home'><img src={Home} alt="" /></Link>
         <UilSetting />
         <img src={Noti} alt="" />
         <img src={Comment} alt="" />
